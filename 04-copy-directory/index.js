8cbb22da9561273bd1c2e4cc3365937960e322be
Mariya Pastushonok <mariyayaromchik@mail.ru>
2021-11-03 20:11:10 +0300

    feat: add solution 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const path = require('path')
+const fs = require('fs')
+const fsp = require('fs').promises
+
+function copyFolder(firstDir, destDir) {
+   fsp.mkdir(destDir, err => {if(err) throw err})
+   fs.readdir(firstDir, 'utf8' , (err, files) => {
+    if(err) throw err;
+    for (let file of files){
+      fs.stat(path.join(firstDir, file), 'utf8', (err, status) => {
+        if(err) throw err;
+        if(status.isFile()) {
+          fs.copyFile(path.join(firstDir, file), path.join(destDir, file), err => {
+            if(err) throw err;
+         })
+      } else {
+         fs.copyFolder(path.join(firstDir, file), path.join(destDir, file), err => {
+          if(err) throw err;
+       })
+    }
+  })
+}
+})
+}   
+copyFolder((path.join(__dirname, 'files')), (path.join(__dirname, 'files-copy')))
+
+
+
 
